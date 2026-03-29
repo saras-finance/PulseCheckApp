44f4f44cd7f9c2b414d0d19d79c7c16f9bf0af77
CP <cprakashagr@gmail.com>
2026-03-29 13:02:59 +0530

    Initial Commit

---
+++ PulseCheck/PulseCheckApp.swift
@@ -0,0 +1,17 @@
+//
+//  PulseCheckApp.swift
+//  PulseCheck
+//
+//  Created by Chandra Prakash on 29/03/26.
+//
+
+import SwiftUI
+
+@main
+struct PulseCheckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
